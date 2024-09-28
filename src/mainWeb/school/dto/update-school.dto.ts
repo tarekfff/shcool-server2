254d3cfbb71f4d3d472e7476f9d2489ee7c093de
Mehdy-AB <76450622+Mehdy-AB@users.noreply.github.com>
2024-09-28 22:35:56 +0100

    Delete src directory

--- src/mainWeb/school/dto/update-school.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSchoolDto } from './create-school.dto';
-
-export class UpdateUserDto extends PartialType(CreateSchoolDto) {}
--- src/mainWeb/school/school.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SchoolService } from './school.service';
-import { CreateSchoolDto } from './dto/create-school.dto';
-
-@Controller('school')
-export class SchoolController {
-}
